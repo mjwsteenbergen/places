d0285a09790916fc0e8b7d1f2691cf6080ecebec
Martijn Steenbergen <m.uses+git@outlook.com>
2022-10-17 22:54:54 +0200

    Different icons

--- src/places.ts
+++ src/places.ts
@@ -1,30 +1,27 @@
-import mapboxgl, { GeolocateControl } from 'mapbox-gl';
-import { Marker, Popup } from 'mapbox-gl';
+import { GeolocateControl, Popup } from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 
 type Place = {
     latitude: number;
     longitude: number;
     name: string;
-    Link?: string;
+    link?: string;
     id: string;
-    Type?: string;
+    type?: Tag;
 }
 
-const places: Place[] = [
-    {
-        latitude: 52.2434633,
-        longitude: 5.6321481,
-        name: "The middle of the Netherlands",
-        id: "not exists",
-    }
-];
+type Tag = {
+    id: string;
+    name: string;
+    color: string;
+}
 
 (mapboxgl as any).accessToken = 'pk.eyJ1IjoibmV3bm90dGFrZW5uYW1lIiwiYSI6ImNsOHhpOTQ0YzAycjAzcHAydGR2bmN5MTYifQ.aNK_O6pCnTXSkxNJ2DcdPQ';
 
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/newnottakenname/cl8xigw5t002s14pbpejhf6ug',
-    zoom: 6,
+    zoom: 4,
     center: [5.55, 52.316667]
 });
 
@@ -39,37 +36,102 @@ map.addControl(location);
 
 const popup = (place: Place) => {
     return new Popup({
-        closeOnMove: true,
         offset: [0, -37]
     }).setHTML(`<h1>${place.name}</h1>
-    <p class="placetype">${place.Type}</p>
-${place.Link ? `<p><a target="_blank" href="${place.Link}">View link</a></p>` : ""}
+    <p class="placetype">${place.type ? place.type.name : ""}</p>
+${place.link ? `<p><a target="_blank" href="${place.link}">View link</a></p>` : ""}
 <p><a target="_blank" href="https://notion.so/${place.id}">Go to Notion</a></p>
 <p><a target="_blank" href="https://www.google.com/maps/search/${place.latitude},${place.longitude}">Get Directions</a></p>`)
 }
 
+const createPlaces = (places: Place[]) => {
+    const group: Record<string, Place[]> = {};
 
-const createPlace = (place: Place) => {
-    const div = document.createElement("div");
-    div.innerHTML = `
-<svg display="block" height="41px" width="27px" viewBox="0 0 27 41"><defs><radialGradient id="shadowGradient"><stop offset="10%" stop-opacity="0.4"></stop><stop offset="100%" stop-opacity="0.05"></stop></radialGradient></defs><ellipse cx="13.5" cy="34.8" rx="10.5" ry="5.25" fill="url(#shadowGradient)"></ellipse><path fill="#3FB1CE" d="M27,13.5C27,19.07 20.25,27 14.75,34.5C14.02,35.5 12.98,35.5 12.25,34.5C6.75,27 0,19.22 0,13.5C0,6.04 6.04,0 13.5,0C20.96,0 27,6.04 27,13.5Z"></path><path opacity="0.25" d="M13.5,0C6.04,0 0,6.04 0,13.5C0,19.22 6.75,27 12.25,34.5C13,35.52 14.02,35.5 14.75,34.5C20.25,27 27,19.07 27,13.5C27,6.04 20.96,0 13.5,0ZM13.5,1C20.42,1 26,6.58 26,13.5C26,15.9 24.5,19.18 22.22,22.74C19.95,26.3 16.71,30.14 13.94,33.91C13.74,34.18 13.61,34.32 13.5,34.44C13.39,34.32 13.26,34.18 13.06,33.91C10.28,30.13 7.41,26.31 5.02,22.77C2.62,19.23 1,15.95 1,13.5C1,6.58 6.58,1 13.5,1Z"></path><circle fill="white" cx="13.5" cy="13.5" r="5.5"></circle></svg>
-<p class="placeName">${place.name}</p>
-`
+    places.forEach(place => {
+        if (!group[place.type?.name ?? "unknown"]) {
+            group[place.type?.name ?? "unknown"] = [];
+        }
+        group[place.type?.name ?? "unknown"].push(place);
+    })
 
-    new Marker({
-        element: div,
-        anchor: 'top-left',
-        offset: [-14, -35.25]
-    }).setLngLat([
-        place.longitude,
-        place.latitude
-    ]).setPopup(popup(place))
-        .addTo(map)
+    Object.keys(group).forEach(key => {
+        createLayer(group[key], key);
+    })
 }
 
-places.forEach(place => {
-    createPlace(place);
-});
+const layerMap = {
+    "Place to visit": "attraction",
+    "Place to eat": "restaurant",
+    "Vacation Highlight": "marker",
+    "unknown": "rocket"
+};
+
+const createLayer = (places: Place[], type: string) => {
+    const mapId = 'places-' + type;
+
+
+    const geojson: GeoJSON.Feature<GeoJSON.Geometry> | GeoJSON.FeatureCollection<GeoJSON.Geometry> = 
+        {
+            'type': 'FeatureCollection',
+            'features': places.map(place => {
+                return {
+                    'type': 'Feature',
+                    'geometry': {
+                            'type': 'Point',
+                            'coordinates': [place.longitude, place.latitude],
+                    },
+                    "properties": place
+                    }
+                })
+        }
+    
+
+    map.addSource(mapId, {
+        type: 'geojson',
+        data: geojson
+    });
+
+    map.addLayer({
+        'id': mapId,
+        'type': 'symbol',
+        'source': mapId,
+        'layout': {
+            'icon-image': (layerMap as any)[type ?? "unkown"] ?? "rocket",
+            'icon-size': 1.5,
+            'icon-allow-overlap': true,
+            'icon-anchor': 'center',
+            'text-optional': true,
+            'text-anchor': 'top',
+            'text-offset': [0, 0.75],
+            'text-field': ['format',
+                ['get', 'name'],
+            ],
+        }
+    });
+
+    map.on('click', (event) => {
+        const features = map.queryRenderedFeatures(event.point, {
+            layers: [mapId]
+        });
+        if (!features.length) {
+            return;
+        }
+
+        const feature = features[0];
+        const coords = (feature.geometry as any).coordinates;
+
+        map.flyTo({
+            center: coords,
+            zoom: map.getZoom() < 14 ? 14 : map.getZoom(),
+        })
+
+        popup(feature.properties as Place)
+            .setLngLat(coords)
+            .addTo(map);
+    });
+}
+
+
 
 type Reply = {
     Result: Place[]
@@ -85,9 +147,7 @@ if (localStorage.getItem("zeuskey")) {
     fetch(`https://zeus-laurentia.azurewebsites.net/api/run/places?token=${localStorage.getItem("zeuskey")}`, {
     }).then(i => i.json()).then(reply => {
         const rep = reply as { Reply: Reply };
-        rep.Reply.Result.forEach(place => {
-            createPlace(place);
-        });
+        createPlaces(rep.Reply.Result);
     })
 } else {
     console.warn("no api key found. not loading in extra items");
